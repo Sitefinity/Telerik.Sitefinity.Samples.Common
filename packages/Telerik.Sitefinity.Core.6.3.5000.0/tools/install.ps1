b207212c8057180e4705f35fd4cd0152702d6cb0
veronicamil <veronica.milcheva@telerik.com>
2014-04-07 14:36:36 +0300

    Revert "NuGet server packages initial commit"
    This reverts commit 113cff8c53e5120aef05bcd0386d2a35fbd01e0d.

--- packages/Telerik.Sitefinity.Core.6.3.5000.0/tools/install.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-	. (Join-Path $toolsPath "CommonPropertyValues.ps1")
-
-	$project.Object.References.Add("$installPath\$assemblyFileRelativePath")
-
-	$project.Save()
