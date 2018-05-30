302a3da054516b2c2d7d1987be8e09f51c225bd3
Nader Dabour <nader.dabour@telerik.com>
2018-05-30 15:33:19 +0300

    Upgraded to SF 11.0.6700.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Progress")]
 [assembly: AssemblyProduct("Progress Sitefinity CMS")]
-[assembly: AssemblyCopyright("Copyright © 2005-2018 Telerik AD. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2005-2018 Telerik EAD. All rights reserved.")]
 [assembly: AssemblyTrademark("Sitefinity")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.2.6600.0")]
-[assembly: AssemblyFileVersion("10.2.6600.0")]
+[assembly: AssemblyVersion("11.0.6700.0")]
+[assembly: AssemblyFileVersion("11.0.6700.0")]
